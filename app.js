2414e7785750ae3c5f36002d9d7077819f67237a
Rlopez00 <investments320@gmail.com>
2024-07-10 19:33:45 -0600

    examen de seguridad segundo parcial

---
+++ app.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { type } = require("os");
+const url_bd = "";
+
+mongoose
+  .connect(url_bd)
+  .then(() => {
+    console.log("Funciona");
+  })
+  .catch((error) => {
+    console.log("No funciona");
+  });
+
+const esquema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  apepat: {
+    type: String,
+  },
+  apemat: {
+    type: String,
+  },
+  numero1: {
+    type: Number,
+  },
+});
+
+const modelo = new mongoose.model("tabla de alumnos", esquema);
+const modelop = new mongoose.model("tabla de maestros", esquema);
+
+modelo.create({
+  name: "Alex",
+  apepat: "Alba",
+  apemat: "Sánchez",
+  numero1: 101203,
+});
